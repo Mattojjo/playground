31b514a862d9bbd5bc1a749cd8ee983d037c8a9d
Mattojjo <leinad9607@gmail.com>
2026-02-08 12:16:59 -0500

    Update HomeButton component styles and remove text label

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -4,11 +4,10 @@ import React from 'react';
 const HomeButton = ({ onGoHome }) => {
   return (
     <button 
-      className="absolute top-2 right-4 px-4 py-1.5 bg-indigo-600 text-white rounded-lg font-bold hover:bg-indigo-700 md:px-6 md:py-3" 
+      className="absolute top-7 left-6 px-4 py-1.5 bg-indigo-600 text-white rounded-lg font-bold hover:bg-indigo-700 hover:scale-125 transition-all duration-200 md:px-6 md:py-3" 
       onClick={onGoHome}
     >
-      <House className="w-6 h-6 inline-block mr-2" />
-      Home
+      <House className="w-6 h-6 inline-block" />
     </button>
   );
 };
